4001249486b15c3a0abcc6eb5e3350e4a793877d
458-nadiraF <nadira.fwzyya@gmail.com>
2021-11-22 19:43:34 +0700

    initial commit

---
+++ php/src/successpage.php
@@ -0,0 +1,48 @@
+<!-- file : successpage.html
+Nama : Nadira Fawziyya Masnur
+NIM  : 18219004 -->
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+	<!-- <script src="https://kit.fontawesome.com/b99e675b6e.js"></script> -->
+    <!-- <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style> -->
+</head>
+<body>
+
+    <div class="wrapper">
+        <div class="form">
+            <div class="title">Login succeeded</div>
+            <button id="submit" onclick=window.location.replace("logout.php") class="btn btn-primary">Sign Out</button>
+        </div>
+        <!-- <div class = "form">
+            
+        </div> -->
+    </div>
+
+</body>
+</html>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Form - 18219004</title>
+	<link rel="stylesheet" href="styles.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+	
+<div class="wrapper">
+	<div class="form">
+		<div class="title">Login succeeded</div>
+	</div>
+</div>
+
+</body>
+</html> -->
